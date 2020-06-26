76c1270608d84b9cc7b38bae7c206185b0bc9244
Ralph Soika <ralph.soika@imixs.com>
2020-06-26 17:02:20 +0200

    implementation
    Issue #4

---
+++ src/main/java/org/imixs/application/ui/view/MyStatusListController.java
@@ -0,0 +1,34 @@
+package org.imixs.application.ui.view;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.imixs.workflow.faces.data.ViewController;
+import org.imixs.workflow.faces.util.LoginController;
+
+@Named
+@ViewScoped
+public class MyStatusListController extends ViewController {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    LoginController loginController;
+
+    /**
+     * Initialize default behavior configured by the BASIC configuration entity.
+     */
+    @Override
+    @PostConstruct
+    public void init() {
+        super.init();
+        this.setQuery("(type:\"workitem\" AND $creator:\"" + loginController.getRemoteUser() + "\")");
+        this.setSortBy("$modified");
+        this.setSortReverse(true);
+        this.setLoadStubs(true);
+
+    }
+
+}
